9c40647c4674dff9497050614b86e8cee87c4c6b
Shota <shota11_s@icloud.com>
2019-02-08 15:12:11 +0800

    Sign up

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :name, presence: true
+    
+    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true,
+                  format: { with:  EMAIL_REGEX },
+                  uniqueness: {case_sensitive: false }
+end
