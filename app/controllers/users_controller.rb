2e487fa50db7f6c74665690574abd281aabd9d60
naturesspring <47379111+naturesspring@users.noreply.github.com>
2019-02-11 15:48:06 +0800

    Profile edit (#7)
    * Profile edit
    
    * Prifile edit2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,20 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def edit
+        @user = User.find(params[:id])
+      end
+    
+      def update
+        @user = User.find(params[:id])
+    
+        if @user.update(user_params)
+          redirect_to @user
+        else
+          render "edit"
+        end
+      end
+
     def show 
         @user = User.find(params[:id])
     end 
