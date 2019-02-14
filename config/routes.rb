82e759794ef14c5a961e31f820d2a35a9f19496b
naturesspring <47379111+naturesspring@users.noreply.github.com>
2019-02-14 14:14:36 +0800

    Categories edit (#10)
    * Categories edit
    
    * Categories edit
    
    * Categories edit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   get "/login", to:"sessions#new"
   delete "/logout", to:"sessions#destroy"
+  
   resources :users, except: :new
 
   namespace :admin do
