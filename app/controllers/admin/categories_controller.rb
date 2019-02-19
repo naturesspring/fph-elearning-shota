cb50ef9346c8bb7d6ed18c106645cfd46982269b
Shota <shota11_s@icloud.com>
2019-02-19 14:15:00 +0800

    Admin authorization

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,8 @@
 class Admin::CategoriesController < ApplicationController
   before_action :admin_user
+  def logged_in?
+    !current_user.nil?
+  end
 
   def index 
     @categories = Category.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
