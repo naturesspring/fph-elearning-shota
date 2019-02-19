546620a52b4fabde7c2064d2bc660ebf507a44c2
Shota <shota11_s@icloud.com>
2019-02-19 17:42:18 +0800

    Lesson page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
-    
     def show 
         @user = User.find(params[:id])
     end 
@@ -38,15 +37,9 @@ class UsersController < ApplicationController
             render "edit"
         end
     end
-
-
     private
         def user_params
             params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
         end
-
-        
-
-        
     end
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ApplicationRecord
- 
     validates :title, :description, presence: true
     has_many :words, dependent: :destroy
 end
